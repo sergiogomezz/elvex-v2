7ec3a358f7bd82956b2d55725c0879f8ddc94a31
sergiogomezz <sergio.gomez.martin10@gmail.com>
2025-12-29 15:49:34 +0100

    names changed

--- src/elvex/core/orchestrator.py
+++ src/elvex/core/orchestrator.py
@@ -1,1 +1,6 @@
 # coordina el trabajo, va llamando a cada agente de manera secuencial
+
+from src.elvex.agents.specifier import TaskSpecifierAgent
+
+def create_workflow(input):
+    TaskSpecifierAgent = TaskSpecifierAgent(input=input)
